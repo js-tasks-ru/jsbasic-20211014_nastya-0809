d1c923ba8cb38be0fdbd7ff06f06e8732a595e55
Anastasia <nastya_0809@mail.ru>
2021-10-27 23:53:49 +0300

    Решение задач из Занятия 3: Функции и массивы

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,13 @@
 function showSalary(users, age) {
-  // ваш код...
+  let resultArr = "";
+
+  for (let i = 0; i < users.length; i++) {
+
+    if (users[i].age <= age) {
+      resultArr += `${users[i].name}, ${users[i].balance}
+`;
+    }
+  }
+
+  return resultArr.slice(0, -1);
 }
