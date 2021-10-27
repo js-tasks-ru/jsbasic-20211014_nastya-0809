3003a1e9411a91d2d7d80787f09acc0a005b0ce6
Anastasia <nastya_0809@mail.ru>
2021-10-27 22:20:26 +0300

    Решение задач из Занятия 2: Объекты

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,15 @@
 function sumSalary(salaries) {
-  // ваш код...
+
+  let resultSalary = 0;
+
+  for (let key in salaries) {
+    // if (typeof salaries[key] === 'number' && !isNaN(salaries[key]) && isFinite(salaries[key])) {
+    if (typeof salaries[key] === 'number' && isFinite(salaries[key])) {
+      resultSalary += salaries[key];
+      console.log("res = ", resultSalary);
+    }
+  }
+
+  return resultSalary;
+
 }
--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,16 @@
 function isEmpty(obj) {
-  // ваш код...
+
+  // 1 ВАРИАНТ
+  for(var key in obj) {
+    return false;
+  }
+
+  return true;
+
+  // // 2 ВАРИАНТ
+  // return Object.keys(obj).length === 0);
+
+  // // 3 ВАРИАНТ - еще вот так можно, но проверка на undefined не проходит (
+  // return JSON.stringify(obj) === '{}'
+
 }
