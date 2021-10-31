44d11eeaf2e7ede6820fe7314ebe853292aab33e
Anastasia <nastya_0809@mail.ru>
2021-10-31 23:36:29 +0300

    Решение задачи 4.1-2

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,26 @@
 function makeFriendsList(friends) {
-  // ваш код...
+
+  // let uList = document.createElement("ul");
+  //
+  // for (let i = 0; i < friends.length; i++) {
+  //
+  //   let uListEl = document.createElement("li");
+  //   uListEl.innerHTML = `${friends[i].firstName} ${friends[i].lastName}`;
+  //   uList.appendChild(uListEl);
+  // }
+  //
+  // document.body.insertAdjacentElement('beforeend', uList);
+
+
+  let uList = document.createElement("ul");
+
+  for (let i = 0; i < friends.length; i++) {
+
+    let uListEl = document.createElement("li");
+    uListEl.innerHTML = `${friends[i].firstName} ${friends[i].lastName}`;
+    uList.appendChild(uListEl);
+  }
+
+  return uList;
+
 }
